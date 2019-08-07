225d79ae5c5a5869a7b344b52196cf3d54f4432a
Kawsar Hussen <kawsarhussen@gmail.com>
2019-08-07 13:10:11 -0400

    logback file added

--- src/main/java/learning/Hello.java
+++ src/main/java/learning/Hello.java
@@ -1,7 +1,13 @@
 package learning;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Hello {
+    private final static Logger log = LoggerFactory.getLogger(Hello.class);
     public static void main(String[] args) {
-        System.out.println("HELLO MAVEN");
+//        System.out.println("HELLO MAVEN");
+        log.info("Hello info");
+        log.info("Hello debug");
     }
 }
